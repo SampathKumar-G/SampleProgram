e73ff96d7472466a16d157270599e864556edf83
SampathKumar-G <sampathk@testvagrant.com>
2020-06-19 08:50:02 +0530

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,9 @@
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.Test;
+
+public class HelloWorld {
+    @Test
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
