1701c0485c6ce8737e817461484fdc010a9fa3e2
SampathKumar-G <sampathk@testvagrant.com>
2020-06-19 10:17:40 +0530

    Testng version changed

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,9 +1,8 @@
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 public class HelloWorld {
     @Test
-    public static void main(String[] args) {
+    public void helloWorld() {
         System.out.println("Hello World");
     }
 }
